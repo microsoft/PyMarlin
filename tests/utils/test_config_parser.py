f3b044e29b6027dfba6ce7645bf8b50cc0b159a8
jsleep <jonrsleep@gmail.com>
2021-06-25 11:58:33 -0700

    test with throwing on 0 yaml files too

--- tests/utils/test_config_parser.py
+++ tests/utils/test_config_parser.py
@@ -78,3 +78,9 @@ class TestConfigParser(TestCase):
             args = Args(**config['test'])
             self.assertTrue(args.test_list_int == [-1, -1, -1])
             self.assertTrue(args.test_list_str == [ 'this', 'is', 'a', 'test', 'list'])
+    
+    def test_dir_configpath_nofiles(self):
+        cmdline_args = ["test", "--config_path", ".//tests//"]
+        with mock.patch('sys.argv', cmdline_args):
+            parser = CustomArgParser(log_level='DEBUG')
+            self.assertRaises(Exception, parser.parse)
