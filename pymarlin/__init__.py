b5e57d20ef5e7755c88db0e9adf2fef75aeefefc
jsleep <jonrsleep@gmail.com>
2022-05-18 17:07:44 -0700

    console handler to set formatter

--- pymarlin/__init__.py
+++ pymarlin/__init__.py
@@ -1,6 +1,6 @@
 """A lightweight library for Deep Learning model training"""
 
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 from pymarlin.core.trainer import (
     TrainerArguments,
     Trainer,
